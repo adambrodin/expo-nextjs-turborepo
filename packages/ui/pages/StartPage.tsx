6e3cae0b215b93828051414ec25ea1fae806d7d2
Adam Brodin <adambrodindeveloper@gmail.com>
2022-01-17 23:37:56 +0100

    feat(packages): add packages directory

---
+++ packages/ui/pages/StartPage.tsx
@@ -0,0 +1,24 @@
+// @generated: @expo/next-adapter@2.1.52
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export function StartPage() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Expo + Next.JS = ❤️</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#6699cc",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+});
