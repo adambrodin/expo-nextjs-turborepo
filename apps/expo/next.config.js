80d8e253897539a433d82b12fe3423ca2232f0d0
Adam Brodin <adambrodindeveloper@gmail.com>
2022-01-17 23:36:26 +0100

    refactor(expo,next): moved next files to /next

--- apps/expo/next.config.js
+++
@@ -1,8 +0,0 @@
-// @generated: @expo/next-adapter@2.1.52
-// Learn more: https://docs.expo.io/guides/using-nextjs/
-
-const { withExpo } = require("@expo/next-adapter");
-
-module.exports = withExpo({
-  projectRoot: __dirname,
-});
