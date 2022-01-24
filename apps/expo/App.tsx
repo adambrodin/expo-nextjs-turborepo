0924a11c3387fe8c12bd54e5d8ff07855bf07483
Adam Brodin <adambrodindeveloper@gmail.com>
2022-01-24 01:59:20 +0100

    feat(expo): add basic navigation

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,4 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { AboutScreen } from 'packages/ui/screens/AboutScreen';
 import { StartScreen } from 'packages/ui/screens/StartScreen';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  return <StartScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={StartScreen}></Stack.Screen>
+        <Stack.Screen name="About" component={AboutScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
